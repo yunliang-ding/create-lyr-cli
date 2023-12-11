0d7fda194b72cf6ef5f6f0422efa549e4d8774f3
xulei <xulei@lei.com>
2023-12-11 21:52:26 +0800

    fixed

--- create/config/index.js
+++ create/config/index.js
@@ -12,8 +12,8 @@ module.exports = {
         {
           name: "base",
           value: {
-            url: "https://github.com/yunliang-ding/react-base-template.git",
-            gitName: "react-base-template",
+            url: "https://github.com/yunliang-ding/lyr-template-base.git",
+            gitName: "lyr-template-base",
             val: "基础版本",
           },
         },
