9f5c7bc123c9dce7af9f82487236c272a1303ab1
xulei <xulei@lei.com>
2023-12-28 19:58:59 +0800

    fixed

--- create/config/index.js
+++ create/config/index.js
@@ -10,18 +10,10 @@ module.exports = {
       name: "type",
       choices: [
         {
-          name: "base",
+          name: "lyr-react",
           value: {
-            url: "https://github.com/yunliang-ding/lyr-template-base.git",
-            gitName: "lyr-template-base",
-            val: "基础版本",
-          },
-        },
-        {
-          name: "pro",
-          value: {
-            url: "https://github.com/yunliang-ding/react-core-form-admin.git",
-            gitName: "react-core-form-admin",
+            url: "https://github.com/yunliang-ding/lyr-react-template.git",
+            gitName: "lyr-react-template",
             val: "全栈开发模版",
           },
         },
